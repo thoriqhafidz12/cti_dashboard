950664576e50b08b0f62a709330e9bb471a4e374
Yusuf <muhammadyusuf667788@gmail.com>
2023-10-17 22:23:21 +0700

    update navbar

--- resources/views/landing/nav.blade.php
+++ resources/views/landing/nav.blade.php
@@ -7,44 +7,52 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="nav navbar-nav   ms-auto mb-2 mb-lg-0">
-          @if (auth()->guest())
-            <li class="nav-item">
-              <a class="btn" href="/login" style="background-color: #a36700;color:white">SIGN IN</a>
-            </li>
-          @else
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">{{ Auth::user()->full_name }}</a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="{{ route('menu') }}">Dashboard</a></li>
+        <div class="row ms-auto">
+          <div class="col-12">
+              <ul class="navbar-nav float-none float-md-end">
+                <li class="nav-item">
+                  <a class="nav-link about-us-link" aria-current="page" href="/">About Us</a>
+                </li>
+                {{-- <li class="nav-item">
+                  <a class="nav-link about-us-link" href="#products">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link about-us-link" href="#values">Values</a>
+                </li> --}}
+                <li class="nav-item">
+                  <a class="nav-link about-us-link" href="{{ route('catalog') }}">Catalog</a>
+                </li>
+      
+                {{--VIEW KERANJANG  --}}
+                @can('admin')
+                <li class="nav-item">
+                  <a class="nav-link about-us-link" href="{{ route('keranjang') }}">Keranjang</a>
+                </li>
+                @endcan
+                {{-- <li class="nav-item">
+                  <a class="nav-link about-us-link" href="#contacts">Contacts</a>
+                </li> --}}
               </ul>
-            </li>
-          @endif
-          <li class="nav-item">
-            <a class="nav-link about-us-link" aria-current="page" href="/">About Us</a>
-          </li>
-          {{-- <li class="nav-item">
-            <a class="nav-link about-us-link" href="#products">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link about-us-link" href="#values">Values</a>
-          </li> --}}
-          <li class="nav-item">
-            <a class="nav-link about-us-link" href="{{ route('catalog') }}">Catalog</a>
-          </li>
+          </div>
+          {{-- menu atas disini bang --}}
+          <div class="col-12 order-md-first">
+              <ul class="navbar-nav float-none float-md-end">
+                @if (auth()->guest())
+                <li class="nav-item">
+                  <a class="btn" href="/login" style="background-color: #a36700;color:white">SIGN IN</a>
+                </li>
+              @else
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">{{ Auth::user()->full_name }}</a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('menu') }}">Dashboard</a></li>
+                  </ul>
+                </li>
+              @endif
+              </ul>
+          </div>
+      </div>
 
-          {{--VIEW KERANJANG  --}}
-          @can('admin')
-          <li class="nav-item">
-            <a class="nav-link about-us-link" href="{{ route('keranjang') }}">Keranjang</a>
-          </li>
-          @endcan
-          {{-- <li class="nav-item">
-            <a class="nav-link about-us-link" href="#contacts">Contacts</a>
-          </li> --}}
-          
-          
-        </ul>
       </div>
     </div>
   </nav>
